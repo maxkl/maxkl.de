2a66d568789384940c1cefacf68f2e3dc217c1d4
maxkl <max@maxkl.de>
2016-02-21 18:19:47 +0100

    Update templates, inline cookie message js

--- public/js/main.js
+++ public/js/main.js
@@ -5,16 +5,4 @@
 
 (function () {
 	"use strict";
-
-	document.getElementById("accept-cookies").addEventListener("click", function (evt) {
-		evt.preventDefault();
-
-		document.cookie = "a=1"
-			+ "; expires=" + new Date(Date.now() + 1000 * 60 * 60 * 24 * 365 * 25)
-			+ "; path=/"
-			+ "; domain=.maxkl.de";
-
-		document.getElementById("cookie-message").style.display = "none";
-	});
-
 })();
