d15a4f40cb5314379ccfbf2695c6e657d7d74a7a
maxkl <max@maxkl.de>
2016-11-05 14:39:59 +0100

    Remove all inline scripts

---
+++ public/js/cookieMessage.js
@@ -0,0 +1,5 @@
+document.getElementById("accept-cookies").addEventListener("click", function (evt) {
+	evt.preventDefault();
+	document.cookie = "a=1; expires=" + new Date(Date.now() + 1000 * 60 * 60 * 24 * 365 * 25) + "; path=/; domain=.maxkl.de";
+	document.getElementById("cookie-message").style.display = "none";
+});
